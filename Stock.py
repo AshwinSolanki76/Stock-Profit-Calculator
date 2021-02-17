428afa75bd2810aadedc2d19e81d52318449002f
[Ashwin Solanki] <[aswinsolanki76@gmail.com]>
2021-02-17 21:12:23 +0530

    Merge branch 'master' of https://github.com/AshwinSolanki76/Stock-Profit-Calculator

--- Stock.py
+++ Stock.py
@@ -1,9 +1,18 @@
 import pandas_datareader.data as stock
-from datetime import date,timedelta
+from datetime import date, datetime,timedelta, timezone
 import numpy
 
 def GetDetail(Date,StockName,Capital):
-    df1=stock.DataReader(StockName[0].upper(),'yahoo',start=Date-timedelta(days=1),end=Date)
-    df2=stock.DataReader(StockName[0].upper(),'yahoo',start=date.today()-timedelta(days=1),end=date.today())
-    NoOfShares=numpy.float32(Capital)/df1.iloc[-1]['Low']
-    return int(NoOfShares*df2.iloc[-1]['High'])
+    if Date.weekday()==5:
+        Date+=timedelta(days=2)
+    elif Date.weekday()==6:
+        Date+=timedelta(days=1)
+    today=date.today()
+    if today.weekday()==0:
+        today-=timedelta(days=2)
+
+    df1=stock.DataReader(StockName[0].upper(),'yahoo',start=Date,end=Date)
+    df2=stock.DataReader(StockName[0].upper(),'yahoo',start=today,end=today)
+    # NoOfShares=numpy.float32(Capital)/df1.iloc[-1]['Low']
+    # return int(NoOfShares*df2.iloc[-1]['High'])
+    return (numpy.float32(df1.iloc[-1]['Low']),numpy.float32(df2.iloc[-1]['High']))
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime,date
 from flask import Flask, render_template, request,url_for
 from Stock import GetDetail
+import numpy
 import os
 
 app = Flask(__name__)
@@ -16,15 +17,21 @@ def result():
         for key, value in request.form.items():
             name[ key ] = value
 
-
         result = request.form
         d=result.to_dict(flat=False)
         datee=datetime(*[int(i) for i in d['Date'][0].split('-')])
         k=GetDetail(datee,d['StockName'],d['Capital'])
-        name['Profit']=GetDetail(datee,d['StockName'],d['Capital'])
+        PriceThen,PriceNow=GetDetail(datee,d['StockName'],d['Capital'])
+        name['Profit']=int((numpy.float32(d['Capital'])/PriceThen)*PriceNow-numpy.float32(d['Capital']))
+        name['PriceThen']=PriceThen
+        name['DateThen']=datee
+        name['DateNow']=date.today()
+        name['PriceNow']=PriceNow
+        name['Growth']=int((PriceNow-PriceThen)*100/PriceThen)
+        print("done")
         return render_template("result.html",result =name)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port)
     # app.run(debug = True)
