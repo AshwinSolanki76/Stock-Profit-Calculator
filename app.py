d768c6ba3e0a2bb3a2ab81e72079a013b7c5ce7e
[Ashwin Solanki] <[aswinsolanki76@gmail.com]>
2021-02-17 21:00:16 +0530

    yoo

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def result():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
     # app.run(debug = True)
